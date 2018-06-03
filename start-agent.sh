2ae9927c916e53dcfe4499616c0da279be4e12cf
ShengRang <541628498@qq.com>
2018-06-04 00:03:11 +0800

    fix

--- start-agent.sh
+++ start-agent.sh
@@ -8,16 +8,16 @@ echo ETCD_URL = $ETCD_URL
 
 if [[ "$1" == "consumer" ]]; then
   echo "Starting consumer agent..."
-  nohup pypy /root/dists/main.py --port=20000 > /root/logs/consumer.log 2>&1 &
+  pypy /root/dists/main.py --port=20000
 elif [[ "$1" == "provider-small" ]]; then
   echo "Starting small provider agent..."
-  nohup pypy /root/dists/pa.py --port=30000 > /root/logs/p-s.log 2>&1 &
+  pypy /root/dists/pa.py --port=30000
 elif [[ "$1" == "provider-medium" ]]; then
   echo "Starting medium provider agent..."
-  nohup pypy /root/dists/pa.py --port=30000 > /root/logs/p-m.log 2>&1 &
+  pypy /root/dists/pa.py --port=30000
 elif [[ "$1" == "provider-large" ]]; then
   echo "Starting large provider agent..."
-  nohup pypy /root/dists/pa.py --port=30000 > /root/logs/p-l.log 2>&1 &
+  pypy /root/dists/pa.py --port=30000
 else
   echo "Unrecognized arguments, exit."
   exit 1
